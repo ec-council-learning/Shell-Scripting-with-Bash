e2197ebda5de2b501a8d7da653cb38232fce75a2
Chris <atnl@protonmail.com>
2021-03-01 19:07:03 -0500

    Add files via upload

---
+++ strs/concat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Initialize the first string variable
+string1="We are learning scripting with Bash! "
+
+#Init  the second string variable
+string2="and having a good time."
+
+string3="BASH and LInx random string here."
+
+#Print after combining both strings
+echo "$string1$string2$string3 "
+
+
+
