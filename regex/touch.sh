0075c517d1656abc3ad2956600702ede01ec2e00
Chris <atnl@protonmail.com>
2021-03-01 20:01:20 -0500

    Add files via upload

---
+++ regex/touch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+touch README.md
+touch requirements.txt
+touch MAKE
+touch install.sh
+touch run.py
+touch data{1..5}.txt
+touch script.php
+touch file.js
+touch image.jpg
+touch image.IMG
+touch cv.tx
+touch nam3
+touch m1ke
+touch 12345
